cc8d2053f74ba51671fd8dd3273fc39decbf08d2
sh95014 <sh95014@example.com>
2022-01-01 21:37:00 +0800

    fix keyboard - control keys - stop forcing caps lock once the user toggles it - prevent sdlframe from sometimes injecting a stray key into the buffer

--- source/frontends/mariani/EmulatorView.mm
+++ source/frontends/mariani/EmulatorView.mm
@@ -27,48 +27,62 @@
     return YES;
 }
 
+// Going by the Apple ][ Reference Manual, the rules are basically:
+// - the number keys are modified by SHIFT only
+// - the letter keys are modified by CTRL only (not true for //e)
+// - 0 is unmodified by CTRL or SHIFT, but that's not relevant to us
+// - special keys like ← or → are sent as control characters
 - (void)keyDown:(NSEvent *)event {
     NSInteger ch = 0;
     
     switch (event.keyCode) {
         case kVK_ANSI_KeypadEnter:
         case kVK_Return:
-            ch = 0x0D;
+            ch = 0x0D;  // ^M
             break;
         case kVK_LeftArrow:
-            ch = 0x08;
+        case kVK_Delete:
+            ch = 0x08;  // ^H
             break;
         case kVK_RightArrow:
-            ch = 0x15;
+            ch = 0x15;  // ^U
             break;
         case kVK_UpArrow:
-            ch = 0x0B;
+            ch = 0x0B;  // ^K
             break;
         case kVK_DownArrow:
-            ch = 0x0A;
-            break;
-        case kVK_Delete:
-            ch = 0x7F;
+            ch = 0x0A;  // ^J
             break;
         case kVK_Escape:
             ch = 0x1B;
             break;
         case kVK_Tab:
-            ch = 0x09;
+            ch = 0x09;  // ^I
             break;
-        case kVK_CapsLock:
-            self.forceCapsLock = NO;
+        default: {
+            // maybe it's already a printable character
+            unichar c = [event.characters characterAtIndex:0];
+            if (event.modifierFlags & NSEventModifierFlagControl) {
+                ch = toupper(c);
+                if (ch >= 'A' && ch <= 'Z') {
+                    ch -= 0x40;  // A -> ^A
+                }
+            }
+            else if (c >= 0x20 && c <= 0x7E) {
+                ch = self.forceCapsLock ? toupper(c) : c;
+            }
             break;
-    }
-    if (ch == 0) {
-        // maybe it's already a printable character
-        unichar c = [event.characters characterAtIndex:0];
-        if (c >= 0x20 && c <= 0x7E) {
-            ch = self.forceCapsLock ? toupper(c) : c;
         }
     }
     if (ch != 0) {
         addKeyToBuffer(ch);
+    }
+}
+
+- (void)flagsChanged:(NSEvent*)event
+{
+    if (event.keyCode == kVK_CapsLock) {
+        self.forceCapsLock = NO;
     }
 }
 
