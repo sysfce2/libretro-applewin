79d8a303bce1c3a026a87b26c3efc7bc466deab5
sh95014 <95387068+sh95014@users.noreply.github.com>
2023-01-30 14:47:18 -0800

    force Xcode to link static libraries for boost and slirp

---
+++ source/frontends/mariani/StaticLibraryWrapper.c
@@ -0,0 +1,15 @@
+//
+//  StaticLibraryWrapper.c
+//  Mariani
+//
+//  Created by sh95014 on 1/30/23.
+//
+
+//  The only purpose of this file is to force Xcode to link the static versions
+//  of external libraries that Mariani needs.
+
+#pragma GCC diagnostic ignored "-Wunused-function"
+
+static void do_nothing()
+{
+}
